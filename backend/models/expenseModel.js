28c20b164ef2adbd31bc4d933d77eeedb934860e
Vishal101022 <raivishal939@gmail.com>
2024-10-25 14:44:21 +0530

    Add sequelize model

--- backend/models/expenseModel.js
+++ backend/models/expenseModel.js
@@ -1,51 +1,31 @@
-const db = require("../util/db");
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../util/db");
 
-// create new expenses
-exports.createExpense = async (expenseData) => {
-  const { amount, description, category } = expenseData;
-  try {
-    const response = await db.query(
-      "INSERT INTO expense (amount, description, category) VALUES (?, ?, ?)",
-      [amount, description, category]
-    );
-    return response;
-  } catch (err) {
-    throw new Error(err);
+const Expense = sequelize.define(
+  "Expense",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "expense",
+    timestamps: false, 
   }
-};
+);
 
-// get all expenses
-exports.getExpenses = async () => {
-  try {
-    const [result, fields] = await db.query(
-      "select id, amount, description, category from expense"
-    );
-    return result;
-  } catch (err) {
-    throw new Error(err);
-  }
-};
-
-// delete  expense
-exports.deleteExpense = async (id) => {
-  try {
-    const response = await db.query("DELETE FROM expense WHERE id = ?", [id]);
-    return response;
-  } catch (err) {
-    throw new Error(err);
-  }
-};
-
-// update expense
-exports.updateExpense = async (id, expenseData) => {
-  const { amount, description, category } = expenseData;
-  try {
-    const [result, fields] = await db.query(
-      "UPDATE expense SET amount = ?, description = ?, category = ? WHERE id = ?",
-      [amount, description, category, id]
-    );
-    return result;
-  } catch (err) {
-    throw new Error(err);
-  }
-};
+module.exports = Expense;
