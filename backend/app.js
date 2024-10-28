7d3466df498ab6cd7c1002434c817588fde4a0fc
Vishal101022 <raivishal939@gmail.com>
2024-10-28 16:08:56 +0530

    Add sequelize relation

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./util/db");
+// routes
 const router = require("./routes/expenseRoutes");
 const userRouter = require("./routes/userRoutes");
 const loginRouter = require("./routes/loginRoutes");
+// models
+const User = require("./models/userModel");
+const Expense = require("./models/expenseModel");
 
 const corsOptions = {
     origin: [
@@ -23,17 +27,22 @@ app.use('/api', router);
 app.use('/', userRouter);
 app.use('/', loginRouter);
 
+
 // test connection
 async function testConnection() {
     try {
         await db.authenticate();
         console.log("Connection has been established successfully.");
+        await db.sync();
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
 }
-
 testConnection();
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
