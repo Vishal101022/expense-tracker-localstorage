6847df8f12b63a32a690077d3e61069351d2fce5
Vishal101022 <raivishal939@gmail.com>
2024-11-03 00:22:09 +0530

    Add isPremium function

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,21 +5,40 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 exports.authMiddleware = async (req, res, next) => {
-    try {
-        const token = req.header("Authorization");
+  try {
+    const token = req.header("Authorization");
 
-        const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-        const userExits = await User.findByPk(payload.userId);
-        
-        if(!userExits){
-            return res.status(401).json({ error: "User not found" });   
-        }
-        req.user = userExits.id;
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.status(401).json({ error: "Unauthorized" });
+    const userExits = await User.findByPk(payload.userId);
+
+    if (!userExits) {
+      return res.status(401).json({ error: "User not found" });
     }
-}
+    req.user = userExits.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
 
+exports.isPremium = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findByPk(payload.userId);
+
+    if (user.isPremiumUser) {
+      console.log("user is premium");
+      next();
+    } else {
+      return res.status(402).json({ error: "Not a premium user" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
