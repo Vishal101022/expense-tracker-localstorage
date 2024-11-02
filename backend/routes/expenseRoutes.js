f67cd229c871d53356378f2152e8b14bd0511ca2
Vishal101022 <raivishal939@gmail.com>
2024-11-03 03:00:19 +0530

    Add route

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -4,8 +4,10 @@ const expenseController = require("../controllers/expenseController");
 const auth = require("../middleware/authMiddleware");
 
 router.post("/expense",auth.authMiddleware, expenseController.createExpense);
-router.get("/expenses",auth.authMiddleware, expenseController.getExpenses);  
-router.delete("/expense/:id", expenseController.deleteExpense);
-router.patch("/expense/:id", expenseController.updateExpense);
+router.get("/expenses", auth.authMiddleware, expenseController.getExpenses);  
+router.get("/totals", auth.authMiddleware, expenseController.getTotals);
+router.delete("/expense/:id",auth.authMiddleware, expenseController.deleteExpense);
+router.patch("/expense/:id", auth.authMiddleware, expenseController.updateExpense);
+router.patch("/totalincome", auth.authMiddleware,expenseController.updateTotalIncome);
 
 module.exports = router;
