ed57fe3dbfcc2b35fcd9181c003e4960a535ecd2
Vishal101022 <raivishal939@gmail.com>
2024-11-03 02:59:39 +0530

    Optimisd the sql query

--- backend/controllers/lederboardController.js
+++ backend/controllers/lederboardController.js
@@ -1,19 +1,11 @@
-const sequelize = require("../util/db");
+const userModel = require("../models/userModel");
 
 exports.leaderboard = async (req, res) => {
   try {
-    const leaderboard = await sequelize.query(
-      `
-      SELECT users.name, 
-             SUM(expenses.amount) AS totalExpense
-      FROM expenses
-      JOIN users ON expenses.UserId = users.id
-      GROUP BY users.id, users.name
-      ORDER BY totalExpense DESC
-    `,
-      { type: sequelize.QueryTypes.SELECT }
-    );
-
+    const leaderboard = await userModel.findAll({
+      attributes: ["id" , "totalexpense" , "name"], 
+      order: [["totalexpense", "DESC"]],
+    })
     res.status(200).json(leaderboard);
   } catch (err) {
     res.status(500).json({ error: err.message });
