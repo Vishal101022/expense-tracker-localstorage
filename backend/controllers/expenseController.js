941ec94c6e07f08cd29302a3b3c5e9abc45a7961
Vishal101022 <raivishal939@gmail.com>
2024-11-03 19:06:09 +0530

    :necktie: fix update business logic

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -4,27 +4,33 @@ const sequelize = require("../util/db");
 
 // create new expenses
 exports.createExpense = async (req, res) => {
-  const t = await sequelize.transaction();
+  const { amount, description, category } = req.body;
+  const UserId = req.user;
+
   try {
-    const { amount, description, category } = req.body;
-    const UserId = req.user;
-    // create new expense
-    const response = await exModel.create(
-      { amount, description, category, UserId },
-      { transaction: t }
-    );
-    // update total expense
     const user = await userModel.findOne({ where: { id: UserId } });
-    const totalExpense = parseFloat(user.totalexpense) + parseFloat(amount);
-    await userModel.update(
-      { totalexpense: totalExpense },
-      { where: { id: UserId } },
-      { transaction: t }
-    );
-    await t.commit();
-    res.status(201).json(response);
+    const updatedTotalExpense =
+      parseFloat(user.totalexpense) + parseFloat(amount);
+
+    const t = await sequelize.transaction();
+    try {
+      const response = await exModel.create(
+        { amount, description, category, UserId },
+        { transaction: t }
+      );
+
+      await userModel.update(
+        { totalexpense: updatedTotalExpense },
+        { where: { id: UserId }, transaction: t }
+      );
+
+      await t.commit();
+      res.status(201).json(response);
+    } catch (err) {
+      await t.rollback();
+      throw err;
+    }
   } catch (err) {
-    await t.rollback();
     res.status(500).json({ error: err.message });
   }
 };
@@ -46,9 +52,7 @@ exports.getExpenses = async (req, res) => {
 };
 
 // delete  expense
-// delete expense
 exports.deleteExpense = async (req, res) => {
-  const t = await sequelize.transaction();
   try {
     const { id } = req.params;
     const UserId = req.user;
@@ -61,60 +65,83 @@ exports.deleteExpense = async (req, res) => {
     const user = await userModel.findOne({ where: { id: UserId } });
     const updatedTotalExpense =
       parseFloat(user.totalexpense) - parseFloat(expense.amount);
+    try {
+      const t = await sequelize.transaction();
+      await userModel.update(
+        { totalexpense: updatedTotalExpense },
+        { where: { id: UserId } },
+        { transaction: t }
+      );
 
-    await userModel.update(
-      { totalexpense: updatedTotalExpense },
-      { where: { id: UserId } },
-      { transaction: t }
-    );
+      await exModel.destroy({ where: { id, UserId } }, { transaction: t });
 
-    await exModel.destroy({ where: { id, UserId } }, { transaction: t });
-
-    res.status(200).json({ message: "Expense deleted successfully" });
-    await t.commit();
+      res.status(200).json({ message: "Expense deleted successfully" });
+      await t.commit();
+    } catch (err) {
+      await t.rollback();
+      throw err;
+    }
   } catch (err) {
-    await t.rollback();
     res.status(500).json({ error: err.message });
   }
 };
 
-// update expense
+// update expense and total expense from user table
 exports.updateExpense = async (req, res) => {
-  const t = await sequelize.transaction();
   try {
     const { id } = req.params;
     const { amount, description, category } = req.body;
-    const response = await exModel.update(
-      { amount, description, category },
-      { where: { id }, transaction: t }
-    );
-    await t.commit();
-    res.status(200).json(response);
+    const oldAmountFromExpense = await exModel.findOne({ where: { id } });
+    if (amount !== oldAmountFromExpense.amount) {
+      const user = await userModel.findOne({ where: { id: req.user } });
+      const oldexpense = await exModel.findOne({ where: { id } });
+      const updatedTotalExpense =
+        parseFloat(user.totalexpense) - parseFloat(oldexpense.amount);
+      const newTotalExpense =
+        parseFloat(updatedTotalExpense) + parseFloat(amount);
+      await user.update({ totalexpense: newTotalExpense });
+    }
+    try {
+      const t = await sequelize.transaction();
+      const response = await exModel.update(
+        { amount, description, category },
+        { where: { id }, transaction: t }
+      );
+      await t.commit();
+      res.status(200).json(response);
+    } catch (err) {
+      await t.rollback();
+      throw err;
+    }
   } catch (err) {
-    await t.rollback();
     res.status(500).json({ error: err.message });
   }
 };
 
 // update total income
 exports.updateTotalIncome = async (req, res) => {
-  const t = await sequelize.transaction();
   try {
     const { amount } = req.body;
     const UserId = req.user;
-    console.log("user id", UserId);
+
     const user = await userModel.findOne({ where: { id: UserId } });
     const totalIncome = parseFloat(user.totalincome) + parseFloat(amount);
-    await userModel.update(
-      { totalincome: totalIncome },
-      { where: { id: UserId } },
-      { transaction: t }
-    );
-    await t.commit();
-    res.status(200).json({ message: "Total income updated successfully" });
-  } catch (error) {
-    await t.rollback();
-    res.status(500).json({ error: error.message });
+    try {
+      const t = await sequelize.transaction();
+
+      await userModel.update(
+        { totalincome: totalIncome },
+        { where: { id: UserId } },
+        { transaction: t }
+      );
+      await t.commit();
+      res.status(200).json({ message: "Total income updated successfully" });
+    } catch (error) {
+      await t.rollback();
+      throw error;
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
 
