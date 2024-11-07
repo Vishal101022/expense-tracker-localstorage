63059a7437ddc543a806988674d948660067c9bf
Vishal101022 <raivishal939@gmail.com>
2024-11-07 21:45:46 +0530

    remove user peference when user log out

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -23,6 +23,7 @@ async function handerFormSubmit(e) {
     const response = await axios.post("http://localhost:3000/login", loginDetails);
     localStorage.setItem("token", response.data.token);
     localStorage.removeItem("currentPage");
+    localStorage.removeItem("limit");
     window.location.href = "../expense/expense.html";
      
   } catch (error) {
