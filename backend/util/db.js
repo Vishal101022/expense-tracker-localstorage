d838c0bd38b2f8786ebc9b68f1f44d68d675f2f7
Vishal101022 <raivishal939@gmail.com>
2024-11-11 16:18:32 +0530

    Add global variable

--- backend/util/db.js
+++ backend/util/db.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const sequelize = new Sequelize("expenses", process.env.DB_USER, process.env.DB_PASS,
+const sequelize = new Sequelize("process.env.DB_NAME", process.env.DB_USER, process.env.DB_PASS,
   {
     dialect: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
   }
 );
 
